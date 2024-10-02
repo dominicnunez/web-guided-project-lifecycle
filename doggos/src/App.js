2f0232104368bad4a04774a29dff7392dadc833b
Dominic A.N. <dominicnunez@gmail.com>
2024-10-02 10:13:06 -0500

    Update build tools, modernize package dependencies, create default export

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>App</h1>
+            </div>
+        )
+    }
+}
